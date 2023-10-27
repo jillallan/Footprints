99cdbf195474024abcc0ceda4a99d7db38c4570c
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-10-27 13:07:38 +0100

    Initial Commit

---
+++ Footprints/FootprintsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootprintsApp.swift
+//  Footprints
+//
+//  Created by Jill Allan on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FootprintsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
