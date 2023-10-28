6ca1979ea1cf48e4cd472218160eb88c1b0309f8
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-10-28 12:50:33 +0100

    Add trip view

---
+++ Footprints/Trips/AddTripView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTripView.swift
+//  Footprints
+//
+//  Created by Jill Allan on 27/10/2023.
+//
+
+import SwiftUI
+
+struct AddTripView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTripView()
+}
