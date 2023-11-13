2936c654c212e7b837b6fef191aa4f96c167fe8d
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-11-13 22:35:25 +0000

    Add Step sample data

--- Footprints/Sample Data/SampleDataGenerator.swift
+++ Footprints/Sample Data/SampleDataGenerator.swift
@@ -11,7 +11,13 @@ import SwiftData
 
 struct SampleDataGenerator {
     @MainActor static func generateSampleData(modelContext: ModelContext) async {
-        modelContext.insert(Trip.bedminsterToBeijing)
+        modelContext.insert(Trip.bedminsterToBeijing)        
         modelContext.insert(Trip.mountains)
+        
+        modelContext.insert(Step.stJohnsLane)
+        modelContext.insert(Step.bedminsterStation)
+        
+        Step.stJohnsLane.trip = Trip.bedminsterToBeijing
+        Step.bedminsterStation.trip = Trip.bedminsterToBeijing
     }
 }
