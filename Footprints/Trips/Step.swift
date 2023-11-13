e72407cfa17269ab6591b8ec29bc5a56e4fa9ad2
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-11-13 15:06:12 +0000

    Add Step Model

--- Footprints/Trips/Step.swift
+++ Footprints/Trips/Step.swift
@@ -5,4 +5,18 @@
 //  Created by Jill Allan on 02/11/2023.
 //
 
-import Foundation
+import SwiftData
+import SwiftUI
+
+@Model
+final class Step {
+    // MARK: - Properties
+    var timestamp: Date
+    
+    var trip: Trip?
+    
+    // MARK: - Initialization
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
