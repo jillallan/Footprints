ddb1520d6d53e744220bc280d83ab66ed9e76ee7
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-11-13 15:05:25 +0000

    Add getWidth view modifier

--- Footprints/Extensions/View+Extension.swift
+++ Footprints/Extensions/View+Extension.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension View {
+    func getWidth(_ width: Binding<CGFloat>) -> some View {
+        modifier(getWidthModifier(width: width))
+    }
+}
+
+struct getWidthModifier: ViewModifier {
+    @Binding var width: CGFloat
+    func body(content: Content) -> some View {
+        content
+            .background(
+                GeometryReader { geometry in
+                    let proxyWidth = geometry.size.width
+                    Color.clear
+                        .task(id: geometry.size.width) {
+                            $width.wrappedValue = max(proxyWidth, 0)
+                        }
+                }
+            )
+    }
+}
