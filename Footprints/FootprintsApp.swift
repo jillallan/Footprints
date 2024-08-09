077cd2b245a1191873eefc8a58977aab2258d5e4
Jill Allan <54241079+jillallan@users.noreply.github.com>
2024-08-09 14:17:19 +0100

    Initial Commit

---
+++ Footprints/FootprintsApp.swift
@@ -0,0 +1,32 @@
+//
+//  FootprintsApp.swift
+//  Footprints
+//
+//  Created by Jill Allan on 09/08/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FootprintsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ FootprintsTests/FootprintsTests.swift
@@ -0,0 +1,16 @@
+//
+//  FootprintsTests.swift
+//  FootprintsTests
+//
+//  Created by Jill Allan on 09/08/2024.
+//
+
+import Testing
+
+struct FootprintsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
