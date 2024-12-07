5081ba2570e03439d692034f8a4a9f9cdb39d7f3
Jill Allan <54241079+jillallan@users.noreply.github.com>
2024-12-07 11:48:30 +0000

    Fix tapped location map

---
+++ Footprints/Location/TappedLocationMap.swift
@@ -0,0 +1,54 @@
+//
+//  TappedLocationMap.swift
+//  Footprints
+//
+//  Created by Jill Allan on 16/11/2024.
+//
+
+import MapKit
+import OSLog
+import SwiftUI
+
+struct TappedLocationMap: View {
+    private let logger = Logger(
+        subsystem: Bundle.main.bundleIdentifier!,
+        category: String(describing: TappedLocationMap.self)
+    )
+    
+    @Binding var mapCameraPosition: MapCameraPosition
+    let currentLocation: CLLocationCoordinate2D?
+    @Binding var tappedLocation: Coordinate?
+    
+    var body: some View {
+        MapReader { mapProxy in
+            Map(position: $mapCameraPosition) {
+                if let currentLocation {
+                    Marker("", coordinate: currentLocation)
+                }
+            }
+            .onTapGesture { position in
+                if let clLocationCoordinate = mapProxy.convert(position, from: .local) {
+                    let _ = logger.debug("Map tapped at: \(String(describing: clLocationCoordinate))")
+                    tappedLocation = Coordinate(from: clLocationCoordinate)
+                }
+            }
+            .onMapCameraChange { context in
+                let _ = logger.debug("Updated map region: \(String(describing: context.region))")
+                mapCameraPosition = .region(context.region)
+            }
+        }
+    }
+}
+
+#Preview {
+    
+    let currentLocation = CLLocationCoordinate2D(latitude: 51.5, longitude: 0.0)
+    let region = MKCoordinateRegion(center: currentLocation, latitudinalMeters: 500, longitudinalMeters: 500)
+    let mapCameraPosition: MapCameraPosition = .region(region)
+    
+    TappedLocationMap(
+        mapCameraPosition: .constant(mapCameraPosition),
+        currentLocation: currentLocation,
+        tappedLocation: .constant(nil)
+    )
+}
